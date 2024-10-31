10bfea9ecbb66f2c08442ff01aee1fa2e8c9cf28
Chirag Bhandakkar <chirag.b@framedx.com>
2024-11-01 00:19:33 +0530

    setup postgres

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/accounts/", include("accounts.urls")),
     path("api/v1/medsplain/", include("medsplain.urls")),
 ]
