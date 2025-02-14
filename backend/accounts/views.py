83be19d861dff21d6000ab9e47f0e10500ec1329
Chirag Bhandakkar <chirag.b@framedx.com>
2025-02-14 22:54:52 +0530

    popup

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,4 @@
+import google.generativeai as genai
 from rest_framework.views import APIView
 
 
@@ -5,6 +6,8 @@ class GoogleAuth(APIView):
     pass
 
 
-def ReportsView(APIView):
-    def get(request):
-        pass
+class ReportsView(APIView):
+    genai.configure(api_key="AIzaSyAfO_84PdUyJCxHCNZVivH4hmbCrm3ToMA")
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content("Explain how AI works")
+    print(response.text)
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  useEffect(() => {
+    if (localStorage.getItem("variable") !== "f") {
+      window.alert("This is a popup");
+    }
+    localStorage.setItem("variable", "f");
+  }, []); // Runs only once when the component mounts
+
+  return (
+    <>
+      <div>Welcome to the Home Page</div>
+    </>
+  );
 }
